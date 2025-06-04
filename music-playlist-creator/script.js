a5a16d1ddb06cc7b220f7a11e60bf7fd78b13145
Emmanuel Aina <emmanuelaina04@meta.com>
2025-06-04 01:09:06 -0700

    First Features Implementation

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,19 @@
+const modal = document.getElementById("playlistModal");
+const span = document.getElementsByClassName("close")[0];
 
+function openModal(playlist) {
+   document.getElementById('playlistName').innerText = playlist.name;
+   document.getElementById('playlistImage').src = playlist.imageUrl;
+   document.getElementById('creatorname').innerText = `Creator Name: ${playlist.artist}`;
+   document.getElementById('artistLineup').innerHTML = `<strong>Song Lineup:</strong> ${playlist.lineup.join(', ')}`;
+   modal.style.display = "block";
+}
+
+span.onclick = function() {
+   modal.style.display = "none";
+}
+window.onclick = function(event) {
+   if (event.target == modal) {
+      modal.style.display = "none";
+   }
+}
