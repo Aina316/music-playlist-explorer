2ddc43644fc0fb2c19520bbc71dd31f5db999ad8
Emmanuel Aina <emmanuelaina04@meta.com>
2025-06-06 16:17:14 -0700

    Core features and stretch features done deadline approaching

--- music-playlist-creator/data/data.js
+++ music-playlist-creator/data/data.js
@@ -6,10 +6,11 @@ let playlists =
 		"playlist_author": "Emmanuel Aina",
 		"playlist_art": "assets/img/pop.jpg",
         "likes": 1,
+        "Date": Date.now()-7,
 		"songs": [
-			{"songtitle":"Blinding Lights","artistname":"The Weeknd","albumname":"After Hours","duration": "1:44", "songArt": "./assets/img/song.png"},
-			{"songtitle":"Levitating","artistname":"Dua Lipa","albumname":"Future Nostalgia","duration": "6:03", "songArt": "./assets/img/song.png"},
-			{"songtitle":"Watermelon Sugar","artistname":"Harry Styles","albumname":"Fine Line","duration": "2:03", "songArt": "./assets/img/song.png"}
+			{"songtitle":"Blinding Lights","artistname":"The Weeknd","albumname":"After Hours","duration": "1:44", "songArt": "./assets/img/one.jpg"},
+			{"songtitle":"Levitating","artistname":"Dua Lipa","albumname":"Future Nostalgia","duration": "6:03", "songArt": "./assets/img/two.jpg"},
+			{"songtitle":"Watermelon Sugar","artistname":"Harry Styles","albumname":"Fine Line","duration": "2:03", "songArt": "./assets/img/three.jpg"}
 		]
 	},
     {
@@ -18,10 +19,11 @@ let playlists =
 		"playlist_author": "Emmanuel Aina",
 		"playlist_art": "assets/img/rock.jpg",
         "likes": 12,
+        "Date": Date.now()-6,
 		"songs": [
-			{"songtitle":"Stairway to Heaven","artistname":"Led Zeppelin","albumname":"Led Zeppelin IV","duration": "3:06", "songArt": "./assets/img/song.png"},
-			{"songtitle":"Back in Black","artistname":"AC/DC","albumname":"Back in Black","duration": "3:21", "songArt": "./assets/img/song.png"},
-			{"songtitle":"Bohemian Rhapsody","artistname":"Queen","albumname":"A Night at the Opera","duration": "3:22", "songArt": "./assets/img/song.png"}
+			{"songtitle":"Stairway to Heaven","artistname":"Led Zeppelin","albumname":"Led Zeppelin IV","duration": "3:06", "songArt": "./assets/img/one.jpg"},
+			{"songtitle":"Back in Black","artistname":"AC/DC","albumname":"Back in Black","duration": "3:21", "songArt": "./assets/img/two.jpg"},
+			{"songtitle":"Bohemian Rhapsody","artistname":"Queen","albumname":"A Night at the Opera","duration": "3:22", "songArt": "./assets/img/three.jpg"}
 		]
 	},
     {
@@ -30,10 +32,11 @@ let playlists =
 		"playlist_author": "Emmanuel Aina",
 		"playlist_art": "assets/img/jazz.jpg",
         "likes": 100,
+        "Date": Date.now()-5,
 		"songs": [
-			{"songtitle":"So What","artistname":"Miles Davis","albumname":"Kind of Blue","duration": "1:42", "songArt": "./assets/img/song.png"},
-			{"songtitle":"Take Five","artistname":"Dave Brubeck Quartet","albumname":"Time Out","duration": "1:56", "songArt": "./assets/img/song.png"},
-			{"songtitle":"My Favorite Things","artistname":"John Coltrane","albumname":"My Favorite Things","duration": "1:42", "songArt": "./assets/img/song.png"}
+			{"songtitle":"So What","artistname":"Miles Davis","albumname":"Kind of Blue","duration": "1:42", "songArt": "./assets/img/one.jpg"},
+			{"songtitle":"Take Five","artistname":"Dave Brubeck Quartet","albumname":"Time Out","duration": "1:56", "songArt": "./assets/img/two.jpg"},
+			{"songtitle":"My Favorite Things","artistname":"John Coltrane","albumname":"My Favorite Things","duration": "1:42", "songArt": "./assets/img/three.jpg"}
 		]
 	},
     {
@@ -42,10 +45,11 @@ let playlists =
 		"playlist_author": "Emmanuel Aina",
 		"playlist_art": "assets/img/blues.jpg",
         "likes": 7,
+        "Date": Date.now()-4,
 		"songs":[
-			{"songtitle":"The Thrill Is Gone","artistname":"B.B. King","albumname":"Completely Well","duration": "2:01", "songArt": "./assets/img/song.png"},
-			{"songtitle":"Cross Road Blues","artistname":"Robert Johnson","albumname":"King of the Delta Blues Singers","duration": "4:21", "songArt": "./assets/img/song.png"},
-			{"songtitle":"Hoochie Coochie Man","artistname":"Muddy Waters","albumname":"His Best","duration": "2:42", "songArt": "./assets/img/song.png"}
+			{"songtitle":"The Thrill Is Gone","artistname":"B.B. King","albumname":"Completely Well","duration": "2:01", "songArt": "./assets/img/one.jpg"},
+			{"songtitle":"Cross Road Blues","artistname":"Robert Johnson","albumname":"King of the Delta Blues Singers","duration": "4:21", "songArt": "./assets/img/two.jpg"},
+			{"songtitle":"Hoochie Coochie Man","artistname":"Muddy Waters","albumname":"His Best","duration": "2:42", "songArt": "./assets/img/three.jpg"}
 		]
 	},
     {
@@ -54,10 +58,11 @@ let playlists =
 		"playlist_author": "Emmanuel Aina",
 		"playlist_art": "assets/img/Funk.jpg",
         "likes": 14,
+        "Date": Date.now()-3,
 		"songs": [
-			{"songtitle":"Superstition","artistname":"Stevie Wonder","albumname":"Talking Book","duration": "1:52", "songArt": "./assets/img/song.png"},
-			{"songtitle":"Give Up the Funk (Tear the Roof off the Sucker)","artistname":"Parliament","albumname":"Mothership Connection","duration": "1:56", "songArt": "./assets/img/song.png"},
-			{"songtitle":"Get Up (I Feel Like Being a) Sex Machine","artistname":"James Brown","albumname":"Sex Machine","duration": "4:22", "songArt": "./assets/img/song.png"}
+			{"songtitle":"Superstition","artistname":"Stevie Wonder","albumname":"Talking Book","duration": "1:52", "songArt": "./assets/img/one.jpg"},
+			{"songtitle":"Give Up the Funk","artistname":"Parliament","albumname":"Mothership Connection","duration": "1:56", "songArt": "./assets/img/two.jpg"},
+			{"songtitle":"Get Up","artistname":"James Brown","albumname":"Sex Machine","duration": "4:22", "songArt": "./assets/img/three.jpg"}
 		]
 	},
     {
@@ -66,10 +71,11 @@ let playlists =
 		"playlist_author": "Emmanuel Aina",
 		"playlist_art": "assets/img/country.jpg",
         "likes": 64,
+        "Date": Date.now()-2,
 		"songs": [
-			{"songtitle":"Jolene","artistname":"Dolly Parton","albumname":"Jolene","duration": "2:00", "songArt": "./assets/img/song.png"},
-			{"songtitle":"Take Me Home, Country Roads","artistname":"John Denver","albumname":"Poems, Prayers & Promises","duration": "2:44", "songArt": "./assets/img/song.png"},
-			{"songtitle":"Ring of Fire","artistname":"Johnny Cash","albumname":"Ring of Fire: The Best of Johnny Cash","duration": "6:32", "songArt": "./assets/img/song.png"}
+			{"songtitle":"Jolene","artistname":"Dolly Parton","albumname":"Jolene","duration": "2:00", "songArt": "./assets/img/one.jpg"},
+			{"songtitle":"Take Me Home, Country Roads","artistname":"John Denver","albumname":"Poems, Prayers & Promises","duration": "2:44", "songArt": "./assets/img/two.jpg"},
+			{"songtitle":"Ring of Fire","artistname":"Johnny Cash","albumname":"ROF: The Best of Johnny Cash","duration": "6:32", "songArt": "./assets/img/three.jpg"}
 		]
 	},
     {
@@ -78,10 +84,11 @@ let playlists =
 		"playlist_author": "Emmanuel Aina",
 		"playlist_art": "assets/img/classical.jpg",
         "likes": 34,
+        "Date": Date.now()-1,
 		"songs": [
-			{"songtitle":"Symphony No.5 in C Minor","artistname":"Ludwig van Beethoven","albumname":"Beethoven: Complete Symphonies","duration": "4:55", "songArt": "./assets/img/song.png"},
-			{"songtitle":"Canon in D Major","artistname":"Johann Pachelbel","albumname":"Pachelbel Canon","duration": "3:54", "songArt": "./assets/img/song.png"},
-			{"songtitle":"The Four Seasons: Spring","artistname":"Antonio Vivaldi","albumname":"Vivaldi: The Four Seasons","duration": "2:41", "songArt": "./assets/img/song.png"}
+			{"songtitle":"Symphony No.5 in C Minor","artistname":"Ludwig van Beethoven","albumname":"Complete Symphonies","duration": "4:55", "songArt": "./assets/img/one.jpg"},
+			{"songtitle":"Canon in D Major","artistname":"Johann Pachelbel","albumname":"Pachelbel Canon","duration": "3:54", "songArt": "./assets/img/two.jpg"},
+			{"songtitle":"The Four Seasons: Spring","artistname":"Antonio Vivaldi","albumname":"Vivaldi: The Four Seasons","duration": "2:41", "songArt": "./assets/img/three.jpg"}
 		]
 	}, 
     {
@@ -90,10 +97,11 @@ let playlists =
 		"playlist_author": "Emmanuel Aina",
 		"playlist_art": "assets/img/neo.jpg",
         "likes": 173,
+        "Date": Date.now(),
 		"songs": [
-			{"songtitle":"Untitled (How Does It Feel)","artistname":"D'Angelo","albumname":"Voodoo","duration": "2:42", "songArt": "./assets/img/song.png"},
-			{"songtitle":"Say Yes","artistname":"Floetry","albumname":"Floetic","duration": "2:22", "songArt": "./assets/img/song.png"},
-			{"songtitle":"Bag Lady","artistname":"Erykah Badu","albumname":"Mama's Gun","duration": "3:23", "songArt": "./assets/img/song.png"}
+			{"songtitle":"Untitled (How Does It Feel)","artistname":"D'Angelo","albumname":"Voodoo","duration": "2:42", "songArt": "./assets/img/one.jpg"},
+			{"songtitle":"Say Yes","artistname":"Floetry","albumname":"Floetic","duration": "2:22", "songArt": "./assets/img/two.jpg"},
+			{"songtitle":"Bag Lady","artistname":"Erykah Badu","albumname":"Mama's Gun","duration": "3:23", "songArt": "./assets/img/three.jpg"}
 		]
 	}
 ]
--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -35,7 +35,6 @@ const loadCards = () =>{
 document.addEventListener("DOMContentLoaded", loadCards)
 
 const createCardElements = (playlist) =>{
-    console.log(playlist)
     const card = document.createElement('div')
     card.className = 'card'
     card.onclick = function (e){
@@ -46,12 +45,13 @@ const createCardElements = (playlist) =>{
     card.innerHTML =
     `<img src="${playlist.playlist_art}">
                 <h2>${playlist.playlist_name}</h2>
-                <h5> Created by ${playlist.playlist_author}</h5>
+                <h5>${playlist.playlist_author}</h5>
                 <div class='like'>
                 <i id="like${playlist.playlistID}" class="fa-regular fa-heart"></i> 
         
                 <p id="like-count${playlist.playlistID}">${playlist.likes}</p>
                 <button class ="delete-btn" data-id=(${playlist.playlistID})>Delete</button>
+                <button class ="edit-btn" data-id=(${playlist.playlistID})>Edit</button>
                 </div>`;
     card.querySelector('.delete-btn').addEventListener('click', function (e) {
         e.stopPropagation();
@@ -63,7 +63,18 @@ const createCardElements = (playlist) =>{
             card.innerHTML = ''
             loadCards()
         }
-    })
+    });
+
+    card.querySelector('.edit-btn').addEventListener('click', function (e) {
+        e.stopPropagation();
+        const name = prompt('Enter new Playlist Name; ', playlist.playlist_name)
+        const author = prompt('Enter new Author Name; ', playlist.playlist_author)
+        if(name!==null && author!== null) {
+            playlist.playlist_name = name.trim() || playlist.playlist_name
+            playlist.playlist_author = author.trim() || playlist.playlist_author
+            loadCards()
+        }  
+    });
     return card;
 }
 
@@ -72,9 +83,9 @@ const createCardElements = (playlist) =>{
 
 function likeEffect(playlist){
 const like = document.getElementById(`like${playlist.playlistID}`);
-console.log("2",like)
+
 const likeCount = document.getElementById(`like-count${playlist.playlistID}`)
-console.log("1000", like)
+
 like.addEventListener('click', function(e){
     e.stopPropagation();
      if (like.classList.contains('fa-regular')) {
@@ -97,19 +108,18 @@ function openModal(playlist) {
    document.getElementById('playlistImage').src = playlist.playlist_art;
    document.getElementById('authorName').innerText = playlist.playlist_author
 
-   loadSongs(playlist.songs)
+   loadSongs(playlist.songs)    
 document.getElementById("shuffle").addEventListener('click', ()=>{
     const songs = playlist.songs
-    console.log("10000", songs)
+    
     const newsongs  = shufflePlaylist(songs)
     const songContainer = document.getElementById("songs-playlist")
-    console.log("111", songContainer)
+    
     songContainer.innerHTML = ''
     for (const song of newsongs){
         let songCard = createSongs(song)
         songContainer.appendChild(songCard)
     }
-
     
 })
    modal.style.display = "block";
@@ -123,8 +133,8 @@ function createSongs(song){
     <h4>${song.songtitle}</h4>
     <h4>${song.artistname}</h4>
     <h4>${song.albumname}</h4>
+    <h4>${song.duration}</h4>
     </div>
-    <p>${song.duration}</p>
     `
     return songPlayCard;
 }
@@ -133,7 +143,6 @@ function loadSongs(songs){
     const playlist = document.getElementById("songs-playlist")
     
     for(const song of songs){
-        
         let songCard = createSongs(song)
         playlist.appendChild(songCard)
     }
@@ -147,4 +156,76 @@ for(let i = songs.length-1; i>=0; i--){
 return songs
 }
 
+function search(){
+const text = document.getElementById('searchPlaylists').value.toLowerCase()
+let length = text.length
+if(text.trim()===''){
+    return;
+}
+const filter = playlists.filter(p => p.playlist_name.toLowerCase().slice(0, length).includes(text.toLowerCase())) || playlists.filter(p => p.playlist_author.toLowerCase().slice(0, length).includes(text.toLowerCase()))
+console.log(filter)
+const cards = document.querySelector('.playlist-cards')
+cards.innerHTML = ''
+for(const card of filter){
+    const searched = createCardElements(card)
+    cards.appendChild(searched)
+    likeEffect(card);
+}
+}
+document.getElementById('search-btn').addEventListener('click', search)
+document.getElementById('clear-btn').addEventListener('click', clear);
 
+function clear(){
+    document.getElementById('searchPlaylists').value = ''
+    loadCards();
+    
+}
+
+function addNewPlaylist(){
+    const name = document.getElementById('playlist-name').value.trim()
+    const author = document.getElementById('author-name').value.trim()
+    const image = document.getElementById('new-image').value.trim()
+
+    if(!name || !author || !image){
+        alert("Properly Fill In Fields")
+        return;
+    }
+    const newPlaylist = {
+        playlistID: Date.now().toString(),
+        playlist_name: name,
+        playlist_author: author,
+        playlist_art: image,
+        likes: 0,
+        songs: []
+    }
+
+    playlists.push(newPlaylist);
+    loadCards();
+
+    document.getElementById('playlist-name').value = ''
+    document.getElementById('author-name').value = ''
+    document.getElementById('new-image').value = ''
+}
+
+
+document.getElementById('sort-options').addEventListener('change', function(){
+    const value = this.value;
+    const sorted = [...playlists]
+
+    if(value==="name"){
+        sorted.sort((a,b)=>a.playlist_name.localeCompare(b.playlist_name))
+    }
+    else if(value==="likes"){
+        sorted.sort((a,b)=>b.likes-a.likes)
+    }
+    else if(value==="date"){
+        sorted.sort((a,b)=>b.Date-a.Date)
+    }
+    const cards = document.querySelector('.playlist-cards')
+    cards.innerHTML = ''
+    for (const playlist of sorted){
+        let cardElement = createCardElements(playlist)
+        cards.appendChild(cardElement)
+        likeEffect(playlist);
+    }
+})
